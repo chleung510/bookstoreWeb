ca2446e86f4978cc88522bda9bd3ca8948385587
chleung510 <jleun006@fiu.edu>
2021-01-12 05:14:49 -0500

    Product management page is created. Privilege roles can add/delete/edit products to/at database.

--- backend/util.js
+++ backend/util.js
@@ -16,4 +16,32 @@ const getToken = (user) => {
     }) // first param: payload, second param: privateKey, third: signOptions(rules).
 }
 
-export { getToken }
+const isAuth = ( req, res, next ) => {
+    // The HTTP Authorization request header contains the credentials 
+    // to authenticate a user agent with a server.
+    const token = req.headers.authorization;
+    
+    if (token) {
+        const onlyToken = token.slice(7, token.length);
+
+        jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+            if (err){ // if error exists, send error message.
+                return res.status(401).send( { msg: "Invalid Token" } );
+            }
+            // if error does not exist, assign decode to req.user.
+            req.user = decode;
+            next();
+            return
+        });
+    } else {
+        return res.status(401).send( { msg: "Token is not supplied." } )
+    }
+}
+
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin){ // if token exist and matched???
+        return next(); // accept the request.
+    }
+    return res.status(401).send({ msg: "Admin Token is not valid." })
+}
+export { getToken, isAuth, isAdmin }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import HomeScreen from './Screens/HomeScreen';
 import CartScreen from './Screens/CartScreen'
 import { useSelector } from 'react-redux';
 import RegisterScreen from './Screens/RegisterScreen';
+import ProductsScreen from './Screens/ProductsScreen';
+import ProfileScreen from './Screens/ProfileScreen';
 
 
 function App() {
@@ -86,6 +88,8 @@ function App() {
             </ul>
         </div>
         </section>
+        <Route path="/products" component={ProductsScreen}/>
+        <Route path="/profile" component={ProfileScreen}/>
         <Route path="/signin" component={SigninScreen}/>
         <Route path="/register" component={RegisterScreen}/>
         <Route path="/product/:id" component={ProductScreen}/>
